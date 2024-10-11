5fdbccb52cf97304fdebfe7b3b034258830dfd3e
Ashutosh Gupta <guptaashutosh584@gmail.com>
2024-10-11 09:11:24 +0530

    some changes done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,8 +4,8 @@ import { Helmet } from 'react-helmet';
 
 const testimonials = [
     {
-        name: 'Shadab',
-        role: 'Fronted Web Developer',
+        name: 'Ashutosh',
+        role: 'Full Stack Developer',
         content:
             'A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.',
        
